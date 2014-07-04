cad14e536363a46a43790d17fb04f9969687fc74
maji-KY <majiky@neco-labo.com>
2014-07-04 13:40:47 +0900

    initial commit

---
+++ tweet-stream-notifier/src/main/java/com/neco_labo/tweet_stream_notifier/Main.java
@@ -0,0 +1,56 @@
+package com.neco_labo.tweet_stream_notifier;
+
+import twitter4j.FilterQuery;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+        StatusListener listener = new StatusListener() {
+            @Override
+            public void onStatus(Status status) {
+                System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+                System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+            }
+
+            @Override
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+                System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+            }
+
+            @Override
+            public void onScrubGeo(long userId, long upToStatusId) {
+                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+            }
+
+            @Override
+            public void onStallWarning(StallWarning warning) {
+                System.out.println("Got stall warning:" + warning);
+            }
+
+            @Override
+            public void onException(Exception ex) {
+                ex.printStackTrace();
+            }
+        };
+        twitterStream.addListener(listener);
+
+        FilterQuery fq = new FilterQuery();
+        fq.track( new String[]{ "六本木" } );
+
+        twitterStream.filter(fq);
+
+	}
+
+}
